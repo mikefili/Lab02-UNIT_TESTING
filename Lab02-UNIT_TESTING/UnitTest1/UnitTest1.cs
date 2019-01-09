31ebb5ed24a372eb70006b7434686e5c4f540d64
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-09 12:01:37 -0800

    initial solution and xUnit test setup complete

---
+++ Lab02-UNIT_TESTING/UnitTest1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Lab02_UNIT_TESTING;
+
+namespace UnitTest1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
