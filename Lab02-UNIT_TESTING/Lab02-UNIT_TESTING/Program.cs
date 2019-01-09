6e9616113cec60c59d58b63b3332918aaa06fa6a
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-09 12:57:01 -0800

    ViewBalance method functional

--- Lab02-UNIT_TESTING/Lab02-UNIT_TESTING/Program.cs
+++ Lab02-UNIT_TESTING/Lab02-UNIT_TESTING/Program.cs
@@ -5,39 +5,51 @@ namespace Lab02_UNIT_TESTING
     class Program
     {
         // declare balance
-        public decimal balance = 5000;
+        public static decimal balance = 5000;
 
         // declare Main method
         static void Main(string[] args)
         {
             // welcome user and present options
             Console.WriteLine("Welcome to your Virtual ATM!");
-            Console.WriteLine("What type of transaction would you like?");
+            Console.WriteLine("****************************");
             Console.WriteLine("1) View Balance");
             Console.WriteLine("2) Withdraw");
             Console.WriteLine("3) Deposit");
             Console.WriteLine("4) Cancel");
-            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("What type of transaction would you like?");
+            Console.WriteLine("****************************************");
+            Console.Write("Selection: ");
+            int pick = int.Parse(Console.ReadLine());
+            switch (pick)
+            {
+                case 1:
+                    ViewBalance();
+                    break;
+            }
         }
 
         public static decimal ViewBalance()
         {
-
+            Console.WriteLine($"Current Balance: ${balance}");
+            Console.ReadLine();
+            return balance;
         } 
 
-        public static decimal Withdraw()
-        {
+        //public static decimal Withdraw()
+        //{
 
-        }
+        //}
 
-        public static decimal Deposit()
-        {
+        //public static decimal Deposit()
+        //{
 
-        }
+        //}
 
-        public static void Cancel()
-        {
+        //public static void Cancel()
+        //{
 
-        }
+        //}
     }
 }
