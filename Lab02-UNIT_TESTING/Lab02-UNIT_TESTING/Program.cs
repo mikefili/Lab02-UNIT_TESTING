bac131acbdb88143bea0b71aa484e8bf3ffd8a35
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-09 12:23:21 -0800

    user welcome complete and methods named

--- Lab02-UNIT_TESTING/Lab02-UNIT_TESTING/Program.cs
+++ Lab02-UNIT_TESTING/Lab02-UNIT_TESTING/Program.cs
@@ -4,9 +4,40 @@ namespace Lab02_UNIT_TESTING
 {
     class Program
     {
+        // declare balance
+        public decimal balance = 5000;
+
+        // declare Main method
         static void Main(string[] args)
         {
-            
+            // welcome user and present options
+            Console.WriteLine("Welcome to your Virtual ATM!");
+            Console.WriteLine("What type of transaction would you like?");
+            Console.WriteLine("1) View Balance");
+            Console.WriteLine("2) Withdraw");
+            Console.WriteLine("3) Deposit");
+            Console.WriteLine("4) Cancel");
+            Console.ReadLine();
+        }
+
+        public static decimal ViewBalance()
+        {
+
+        } 
+
+        public static decimal Withdraw()
+        {
+
+        }
+
+        public static decimal Deposit()
+        {
+
+        }
+
+        public static void Cancel()
+        {
+
         }
     }
 }
